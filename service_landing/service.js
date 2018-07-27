c884326cac716fb2a2b9e866fb89a63f96e03aa7
j-github01 <raghibjuned@gmail.com>
2018-07-27 12:30:58 +0530

    landing page recreated

---
+++ service_landing/service.js
@@ -0,0 +1,25 @@
+document.onload(
+    showTab(0)
+)
+function showTab(n) 
+{
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  var b = document.getElementsByClassName("step");
+  for (let i = 0; i < x.length; i++) {
+      x[i].style.display="none";
+  }
+  x[n].style.display = "block";
+  b[n-1].className+=" finish"
+  //... and run a function that will display the correct step indicator:
+}
+
+$(document).ready(function () {
+    $('.your-class').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+
